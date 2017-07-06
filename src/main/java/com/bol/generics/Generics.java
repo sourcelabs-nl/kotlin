67a5a4ac23e23809eb09e396683b46913bd64b0f
Jarno Walgemoed <jwalgemoed@bol.com>
2017-07-06 09:29:40 +0200

    Fixes, more generics

--- src/main/java/com/bol/generics/Generics.java
+++ src/main/java/com/bol/generics/Generics.java
@@ -1,11 +1,21 @@
 package com.bol.generics;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Kotlin generic types add in/out covariant/contravariant types to provide compiler hints for safe/unsafe operations.
  * That said, Kotlin does not contain raw types, meaning that the example below with the unsafe raw type is not possible
  * in Kotlin.
  */
 public class Generics {
+    // Wildcard declaration at call site constraining the allowed generic types accepted by this method
+    public static void wildcardGeneric(List<? extends Number> numbers) {
+    }
+
+    // Unconstrained (the <?> can be omitted but its good practice to explicitly specify it)
+    public static void wildcardGenericUnconstrained(List<?> objects) {
+    }
 
     public static void main(String[] args) {
         final GenericClass<String> genericClass = new GenericClass();
@@ -16,6 +26,15 @@ public class Generics {
         // Bounded generics
         GenericBounded<Integer> integerGenericBounded = new GenericBounded();
         //GenericBounded<String> stringGenericBounded = new GenericBounded(); // Compiler error, String no subclass of number
+
+        // Wildcards
+        wildcardGeneric(new ArrayList<Integer>());
+        wildcardGeneric(new ArrayList<Double>());
+        //wildcardGeneric(new ArrayList<Object>()); // Compiler error
+
+        // Unconstrained -- anything goes
+        wildcardGenericUnconstrained(new ArrayList<String>());
+        wildcardGenericUnconstrained(new ArrayList<Integer>());
     }
 }
 
@@ -23,14 +42,22 @@ public class Generics {
  * Unbounded generics class
  */
 class GenericClass<T> {
-    public T getThing() { return null; }
-    public void addThing(T t) {  }
+    public T getThing() {
+        return null;
+    }
+
+    public void addThing(T t) {
+    }
 }
 
 /**
  * Bounded generics class
  */
 class GenericBounded<T extends Number> {
-    public T getThing() { return null; }
-    public void addThing(T t) {  }
+    public T getThing() {
+        return null;
+    }
+
+    public void addThing(T t) {
+    }
 }
