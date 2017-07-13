124da606048982159e9031c3009d04ef7163276d
Stephan Oudmaijer <soudmaijer@bol.com>
2017-07-14 00:30:05 +0200

    implements, nullable

---
+++ kotlin-workshop/src/main/kotlin/nl/sourcelabs/kotlin/KotlinImplementsJava.kt
@@ -0,0 +1,12 @@
+package nl.sourcelabs.kotlin
+
+import java.awt.event.ActionEvent
+import java.awt.event.ActionListener
+
+class KotlinImplementsJava : ActionListener {
+
+    override fun actionPerformed(e: ActionEvent?) {
+        // print(e.source)
+    }
+
+}
