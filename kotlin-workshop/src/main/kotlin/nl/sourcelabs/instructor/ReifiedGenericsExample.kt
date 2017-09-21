2dc3132fd76b081257a58bb80b916f11a073e5a6
Stephan Oudmaijer <soudmaijer@bol.com>
2017-09-21 10:03:12 +0200

    Simplified the reified generics example.

--- kotlin-workshop/src/main/kotlin/nl/sourcelabs/instructor/ReifiedGenericsExample.kt
+++ kotlin-workshop/src/main/kotlin/nl/sourcelabs/instructor/ReifiedGenericsExample.kt
@@ -2,52 +2,48 @@ package nl.sourcelabs.instructor
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.KotlinModule
-import org.junit.Test
 import org.junit.Assert
+import org.junit.Test
 
-val objectMapper = ObjectMapper().registerModule(KotlinModule())
-
-/**
- * Some message envelope containing a json payload.
- */
-class MessageEnvelope(val payload: String)
-
-/**
- * Representation of the json message payload.
- */
 class Message(val message: String)
 
-/**
- * Broken version without the reified generics
- * Does not work because the type information on the class is not
- * available at runtime due to type erasure.
- */
-//class MessageHandler<out T> {
-//
-//    // Does not work because of type erasure
-//    fun handleMessage(message: MessageEnvelope): T {
-//        return objectMapper.readValue(message.payload, T::class.java)
-//    }
-//}
+class MessageConverter {
 
-/**
- * Instead of defining the type on the class we add the <reified T> to handleMessage.
- * Now the compiler can reify the type for the handleMessage at the invocation point.
- */
-class MessageHandler {
+    val objectMapper = ObjectMapper().registerModule(KotlinModule())
 
-    // Does work because all of the type information is in the inline function call
-    inline fun <reified T> handleMessage(message: MessageEnvelope): T {
-        return objectMapper.readValue(message.payload, T::class.java)
+    /**
+     * Version without the reified generics, we need to pass the type Class because the T (type) info is not usable at runtime due to type erasure.
+     */
+    fun <T> convert(jsonMessage: String, clazz: Class<T>): T {
+        return objectMapper.readValue(jsonMessage, clazz)
+    }
+
+    /**
+     * Instead of passing the class we add the <reified T> to the function declaration.
+     * Now the compiler can reify the type for the convert at the invocation point.
+     * Note that this only works for inline functions!
+     */
+    inline fun <reified T> convert(jsonMessage: String): T {
+        return objectMapper.readValue(jsonMessage, T::class.java)
     }
 }
 
 class MessageHandlerTest {
+
     @Test
     fun testHandleMessage() {
-        val messageEnvelope = MessageEnvelope(payload = """{ "message": "Hello world!" }""")
+        val jsonString = """{ "message": "Hello world!" }"""
 
-        val m = MessageHandler().handleMessage<Message>(messageEnvelope)
+        val m = MessageConverter().convert(jsonString, Message::class.java)
+
+        Assert.assertEquals("Hello world!", m.message)
+    }
+
+    @Test
+    fun testHandleMessageWithReified() {
+        val jsonString = """{ "message": "Hello world!" }"""
+
+        val m = MessageConverter().convert<Message>(jsonString)
 
         Assert.assertEquals("Hello world!", m.message)
     }
